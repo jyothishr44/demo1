893954682023b3b17c8825feab57936acd7e8b03
jyothishr44 <155710282+jyothishr44@users.noreply.github.com>
2024-01-05 12:11:45 +0530

    Add files via upload

---
+++ ArrayForLoop.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+class ArrayForLoop
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a[]={12,4,5,2,5};
+System.out.println("Accessing elements of array:");
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
